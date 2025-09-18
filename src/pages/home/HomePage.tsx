1dbfa756c0db41a157a81ec190fd21eacd66e709
Quoc Huy <huydqds180257@fpt.edu.vn>
2025-09-18 17:52:13 +0700

    [PET-08][FE][feat] done chatbox AI

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+import ChatBot from "../../features/chatbot-ai/Chatbot";
+import FloatingChatButton from "../../features/chatbot-ai/FloatingChatButton";
 import HeroSection from "./components/HeroSection";
 import ServiceSection from "./components/ServiceSection";
 
 export const HomePage = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <HeroSection />
       <ServiceSection />
+      <>
+        <ChatBot
+          open={open}
+          onClose={() => setOpen(false)}
+          backendUrl="https://your-backend.com/chat" // ← endpoint của bạn
+          buildPayload={(text, history) => ({ prompt: text, history })}
+        />
+        <FloatingChatButton onClick={() => setOpen(true)} />
+      </>
     </>
   );
 };
 
 export default HomePage;
-
